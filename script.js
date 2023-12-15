6ec9ec0b0eb07dc145f5cce5a63c464649f087ef
Abinesh P <150216317+abinesh-a7@users.noreply.github.com>
2023-12-15 19:49:50 +0530

    file uploaded

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuicon = document.getElementById("menu-icon");
+let navbar = document.getElementById("navbar");
+
+menuicon.addEventListener("click", () => {
+  navbar.classList.toggle("showmenu");
+});
+
